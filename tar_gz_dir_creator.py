648acaab63cdb761c8ea4689fdb527b8f095caf4
merry <merry>
2014-06-15 18:47:58 +0400

    added script for making tar archive from some directory

---
+++ tar_gz_dir_creator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import tarfile
+
+def creator(src, dst):
+	tar = tarfile.open(dst, "w|gz")
+	for root, dir, files in os.walk(src):
+		for file in files:
+			fullpath = os.path.join(root, file)
+			tar.add(fullpath)
+	tar.close()
+
+if __name__ == '__main__':
+	if len(sys.argv) != 2:
+		print "Usage " + "./" + os.path.basename(__file__) + " SRC DST"
+	else:
+		src = sys.argv[1]
+		dst = sys.argv[2]
+		creator(src, dst)
