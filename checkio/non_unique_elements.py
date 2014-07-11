4591eab168746b88f923cc928781fa6ef8a98919
Igor Biryulin <ibiryulin@qsoft.ru>
2014-07-11 18:50:03 +0400

    added solution for non_unique_elements mission on http://www.checkio.org

---
+++ checkio/non_unique_elements.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+def checkio(data):
+    result_list = data[:]
+    for element in data:
+        if data.count(element) == 1:
+            result_list.remove(element)
+   
+    return result_list
+
+if __name__ == "__main__":
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert isinstance(checkio([1]), list), "The result must be a list"
+    assert checkio([1, 2, 3, 1, 3]) == [1, 3, 1, 3], "1st example"
+    assert checkio([1, 2, 3, 4, 5]) == [], "2nd example"
+    assert checkio([5, 5, 5, 5, 5]) == [5, 5, 5, 5, 5], "3rd example"
+    assert checkio([10, 9, 10, 10, 9, 8]) == [10, 9, 10, 10, 9], "4th example"
